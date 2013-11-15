7697e9a9a1b427b19a48c4e2662ab380e47dd793
Michael Jackson <mjijackson@gmail.com>
2013-11-15 08:20:34 -0800

    Cleanup job handling code

--- test/test-helper.js
+++ test/test-helper.js
@@ -3,3 +3,10 @@ Firebase = require('firebase');
 firework = require('../lib');
 
 BASE_REF = new Firebase('https://firework-tests.firebaseio.com');
+
+// Warm up the connection before running tests.
+before(function (done) {
+  BASE_REF.once('value', function () {
+    done();
+  });
+});
