5aac967236a2ebda425150d09098d6c64a5323ee
Michael Jackson <mjijackson@gmail.com>
2014-04-03 08:08:08 -0700

    Add firework.create* methods

--- modules/index.js
+++ modules/index.js
@@ -2,3 +2,15 @@ exports.version = require('../package').version;
 exports.Queue  = require('./queue');
 exports.Runner = require('./runner');
 exports.Worker = require('./worker');
+
+exports.createQueue = function (ref) {
+  return new exports.Queue(ref);
+};
+
+exports.createRunner = function (createWorker) {
+  return new exports.Runner(createWorker);
+};
+
+exports.createWorker = function (queue, performJob) {
+  return new exports.Worker(queue, performJob);
+};
